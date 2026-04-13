b8c54dda8411dc96685c586ec13cb34ffc59e3f2
IT-GGbond <2564183149@qq.com>
2026-04-13 12:41:39 +0800

    feat: 导入react-markdown库

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,21 @@ import ChatRecord from './components/ChatRecord'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const mdList = [
+    '简单来说，**`React.FC` 是 TypeScript 的类型定义，而 `import React` 的省略是 React 17 引入的新特性**。',
+    '# Hello, world!\n\nThis is a simple paragraph with some **bold** text.'
+  ]
+  const [llmRes, setLlmRes] = useState(mdList[0]);
+  function next() {
+    setCount(count + 1);
+    setLlmRes(mdList[count % mdList.length])
+  }
   return (
     <>
       yes{count}
       <div className="btn" onClick={() => setCount(count + 1)}>点我加一</div>
-      <ChatRecord></ChatRecord>
+      <div className="btn" onClick={next}>下一个</div>
+      <ChatRecord content={llmRes}></ChatRecord>
       <Chat></Chat>
     </>
   )
--- src/components/ChatRecord/index.tsx
+++ src/components/ChatRecord/index.tsx
@@ -1,8 +1,14 @@
 import './index.css'
-function ChatRecord() {
+import ReactMarkdown from 'react-markdown';
+interface ChatRecordProps {
+    content: string;
+}
+
+function ChatRecord({ content }: ChatRecordProps) {
     return (
         <div className="record-wrap">
             聊天记录
+            <ReactMarkdown>{content}</ReactMarkdown>
         </div>
     )
 }
